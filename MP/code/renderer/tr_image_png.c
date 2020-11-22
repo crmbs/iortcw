19ad2b610e6d8e6e4729311a749867ce67b612b4
crmbs <richwright20@gmail.com>
2020-11-22 12:37:07 +0000

    Updating video command

--- MP/code/renderer/tr_image_png.c
+++ MP/code/renderer/tr_image_png.c
@@ -19,7 +19,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 ===========================================================================
 */
 
-#include "tr_common.h"
+#include "tr_local.h"
 
 #include "../qcommon/puff.h"
 #include "../zlib/zlib.h"
