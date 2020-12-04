3eafb960614845b009c3e7fe7a04f41d4cd25efa
crmbs <richwright20@gmail.com>
2020-12-04 11:51:10 +0000

    demo_saveHud

--- MP/code/rend2/tr_backend.c
+++ MP/code/rend2/tr_backend.c
@@ -2064,12 +2064,7 @@ void RB_ExecuteRenderCommands( const void *data ) {
 			data = RB_SetColor( data );
 			break;
 		case RC_STRETCH_PIC:
-			//if (demo_saveHud->integer) {
-				data = RB_SkipRenderCommand(data);
-			//}
-			//else {
-			//	data = RB_StretchPic(data);
-			//}
+			data = RB_StretchPic(data);
 			break;
 		case RC_ROTATED_PIC:
 			data = RB_RotatedPic( data );
